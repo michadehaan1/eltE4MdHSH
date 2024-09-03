b63fce609114981af7ae06309acb526263241c9e
Stefan Helmantel <s.helmantel@tiepie.nl>
2024-09-03 21:18:48 +0200

    Changed the greeting name.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Micha",
+                        name = "Stefan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
